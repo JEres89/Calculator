4fccb6171b59f7697487ca77d98ca558e2366b53
JEres89 <jens.eresund@gmail.com>
2022-03-24 14:23:24 +0100

    Första uppgiften klar

---
+++ Calculator/Maths.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+	static class Maths
+	{
+
+		private static readonly String[] Verb =
+		{
+			"adderas",
+			"subtraheras",
+			"multipliceras",
+			"divideras"
+		};
+		private static readonly char[] Sign =
+		{
+			'+',
+			'-',
+			'*',
+			'/'
+		};
+
+		private static readonly Predicate<(decimal, decimal)>[] MathFuncs = { Addition, Subtraction, Multiplication, Division };
+
+		public static void SimpleMath(int fun)
+		{
+			decimal a, b;
+			Console.WriteLine("Skriv de två tal som skall {0} (OBS! decimaltecken skrivs med komma ','):", Verb[fun]);
+
+			a = ReadNumber();
+			b = ReadNumber();
+
+			if (MathFuncs[fun]( (a, b) ))
+			{
+				(decimal, int, decimal, decimal) result = results[results.Count-1];
+				Console.WriteLine("{0} {1} {2} = {3}", result.Item1, Sign[result.Item2], result.Item3, result.Item4);
+			}
+			else
+			{
+				Console.WriteLine("Division med noll är odefinierat");
+			}
+		}
+
+		private static decimal ReadNumber()
+		{
+			decimal input;
+			while (!decimal.TryParse(Console.ReadLine(), out input))
+			{
+				Console.WriteLine("Inte ett tal, försök igen");
+			}
+			return input;
+		}
+
+
+		static List<(decimal, int, decimal, decimal)> results = new();
+
+		private static bool Addition((decimal a, decimal b) nums)
+		{
+			decimal result = nums.a + nums.b;
+
+			results.Add((nums.a, 0, nums.b, result));
+
+			return true;
+		}
+		public static bool Subtraction((decimal a, decimal b) nums)
+		{
+			decimal result = nums.a - nums.b;
+
+			results.Add((nums.a, 1, nums.b, result));
+
+			return true;
+		}
+		public static bool Multiplication((decimal a, decimal b) nums)
+		{
+			decimal result = nums.a * nums.b;
+
+			results.Add((nums.a, 2, nums.b, result));
+
+			return true;
+		}
+		public static bool Division((decimal a, decimal b) nums)
+		{
+			if (nums.b == 0)
+			{
+				return false;
+			}
+
+			decimal result = nums.a / nums.b;
+
+			results.Add((nums.a, 3, nums.b, result));
+
+			return true;
+		}
+	}
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using 
 
 namespace Calculator
 {
@@ -8,7 +7,46 @@ namespace Calculator
 		static void Main(string[] args)
 		{
 
-			Console.WriteLine("Hello World!");
+			while (true)
+			{
+				Console.Clear();
+				Console.WriteLine("-- Calculator -----------\n");
+				Console.WriteLine("  1. Addition");
+				Console.WriteLine("  2. Subtraktion");
+				Console.WriteLine("  3. Multiplikation");
+				Console.WriteLine("  4. Division");
+				Console.WriteLine("  5. Friform");
+				Console.WriteLine("  6. Exit");
+				Console.WriteLine("-------------------------\n");
+				Console.Write("Välj funktion # + enter: ");
+
+				String s = Console.ReadLine();
+
+				int choice = s.Length == 0 ? 0 : Char.IsDigit(s[0]) ? (int)Char.GetNumericValue(s[0]) : 0 ;
+
+				switch (choice)
+				{
+					case 0:
+						Console.WriteLine("Ogiltigt val!");
+						break;
+					case 1:
+					case 2:
+					case 3:
+					case 4:
+						Maths.SimpleMath(choice - 1); //index correction
+						break;
+					case 5:
+						Console.WriteLine("Ej implementerad");
+						break;
+					case 6:
+						return;
+					default:
+						break;
+				}
+
+				Console.WriteLine("\n\nPress the any key.");
+				Console.ReadKey();
+			}
 		}
 	}
 }
